d3a7fdea13402e3cbfe7b47516ce71cec52bf10b
Mariusz Foltak <mariusz.foltak@oracle.com>
2021-08-23 19:09:19 +0200

    Implement PESEL validator

--- src/main/java/pl/foltak/polishidnumbers/pesel/PeselValidator.java
+++ src/main/java/pl/foltak/polishidnumbers/pesel/PeselValidator.java
@@ -2,6 +2,43 @@ package pl.foltak.polishidnumbers.pesel;
 
 public class PeselValidator {
     public boolean isValid(String pesel) {
-        return false;
+        if (pesel.length() != 11) {
+            return false;
+        }
+        if (!pesel.matches("\\d+")) {
+            return false;
+        }
+        if (!hasCorrectCheckDigit(pesel)) {
+            return false;
+        }
+        if (!hasCorrectBirthDate(pesel)) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean hasCorrectBirthDate(String pesel) {
+        int month = Integer.parseInt(pesel.substring(2, 4));
+        return ((month >= 1 && month <= 12) ||
+                (month >= 21 && month <= 32) ||
+                (month >= 41 && month <= 52) ||
+                (month >= 61 && month <= 72) ||
+                (month >= 81 && month <= 92));
+    }
+
+    private boolean hasCorrectCheckDigit(String pesel) {
+        int[] weights = {1, 3, 7, 9, 1, 3, 7, 9, 1, 3, 1};
+        int checkDigit = Character.getNumericValue(pesel.charAt(10));
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum += Character.getNumericValue(pesel.charAt(i)) * weights[i];
+        }
+        int modulo = sum % 10;
+        int calculatedCheckDigit = 10 - modulo;
+
+        if (modulo != 0 && calculatedCheckDigit != checkDigit) {
+            return false;
+        }
+        return true;
     }
 }
--- src/test/java/pl/foltak/polishidnumbers/pesel/PeselValidatorTest.java
+++ src/test/java/pl/foltak/polishidnumbers/pesel/PeselValidatorTest.java
@@ -1,24 +1,83 @@
 package pl.foltak.polishidnumbers.pesel;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class PeselValidatorTest {
 
     @Test
-    public void peselIsValid() {
-        String test = "v1.10.1-32-ga03dea36";
-        Pattern pattern = Pattern.compile("v(\\d+(\\.\\d+)+).*");
-        Matcher matcher = pattern.matcher(test);
-        if (matcher.find()) {
-            assertEquals("1_10_1", matcher.group(1).replace('.', '_'));
-        } else {
-            fail();
-        }
+    public void pesel_should_not_be_null() {
+        assertThrows(NullPointerException.class, () -> {
+            new PeselValidator().isValid(null);
+        });
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "abc", "toooooolongstring"})
+    public void pesel_should_be_11_characters_long(String string) {
+        assertThat(new PeselValidator().isValid(string), equalTo(false));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"aaaaaaaaaaa", "1234567890a", "a1234567890"})
+    public void pesel_should_contains_only_numbers(String string) {
+        assertThat(new PeselValidator().isValid(string), equalTo(false));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "61020335564",
+            "55080582722",
+            "54030186184",
+            "69021269721",
+            "84052133373",
+            "54070643896",
+            "87062151182",
+            "52102544331",
+            "99122584564",
+            "92060671492"
+    })
+    public void pesel_should_have_correct_check_digit(String string) {
+        assertThat(new PeselValidator().isValid(string), equalTo(false));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "53000397537",
+            "48131587553",
+            "85200695557",
+            "91330733933",
+            "49400268349",
+            "92531214389",
+            "47601028566",
+            "64732879430",
+            "53800314240",
+            "52930717289"
+    })
+    public void pesel_should_have_correct_birth_date(String string) {
+        assertThat(new PeselValidator().isValid(string), equalTo(false));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "67040275410",
+            "49112185311",
+            "72030586142",
+            "88082595483",
+            "64081859634",
+            "46041031575",
+            "59102843826",
+            "47090271277",
+            "53021662818",
+            "74032698429"
+    })
+    public void pesel_is_valid(String string) {
+        assertThat(new PeselValidator().isValid(string), equalTo(true));
     }
 
 }
